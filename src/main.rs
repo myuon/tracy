04dc8f96ff8707c61531d1905dadbfad93bb0018
myuon <ioi.joi.koi.loi@gmail.com>
2019-02-12 12:51:40 +0900

    feat: Screen object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+use std::io::{self, BufWriter, Write};
+use std::fs;
+
+#[derive(Clone, Copy)]
+struct Color(u8, u8, u8);
+
+impl Color {
+    fn red(&self) -> u8 {
+        self.0
+    }
+
+    fn green(&self) -> u8 {
+        self.1
+    }
+
+    fn blue(&self) -> u8 {
+        self.2
+    }
+}
+
+struct Screen {
+    width: i32,
+    height: i32,
+}
+
+impl Screen {
+    fn write(&self, file_path: &str) -> io::Result<()> {
+        let pixels = self.calculate();
+
+        self.write_ppm(file_path, pixels)
+    }
+
+    fn calculate(&self) -> Vec<Color> {
+        let mut pixels = vec![Color(255,255,255); (self.width * self.height) as usize];
+
+        pixels
+    }
+
+    fn write_ppm(&self, file_path: &str, pixels: Vec<Color>) -> io::Result<()> {
+        let mut file = BufWriter::new(fs::File::create(file_path).unwrap());
+        file.write(format!("P3\n{} {}\n255\n", self.width, self.height).as_bytes())?;
+
+        let pixel_array = pixels.as_slice();
+
+        for j in 0..self.height {
+            for i in 0..self.width {
+                let c = pixel_array[(i + j * self.width) as usize];
+
+                file.write(format!(
+                    "{} {} {}\n",
+                    c.red(),
+                    c.green(),
+                    c.blue(),
+                ).as_bytes())?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let screen = Screen{
+        width: 200,
+        height: 150
+    };
+
+    screen.write("./out/image.ppm").unwrap();
 }
